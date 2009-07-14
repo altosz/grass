4cdd9ccf4ce239da28d5216e1dc8053ec372e268
altosz <altos.z@gmail.com>
2009-07-14 23:02:36 +0400

    Stub for grid-img added

--- scripts/GridImg.groovy
+++ scripts/GridImg.groovy
@@ -1,7 +1,16 @@
 includeTargets << grailsScript("Init")
 
 target(main: "The description of the script goes here!") {
-    // TODO: Implement script here
+	gridImg()
+}
+
+target(gridImg: "Generate grid.png") {
+	println """
+Not implemented yet.
+You may get grid.png for your css in http://kematzy.com/blueprint-generator/
+Place it under ./web-app/images
+To show grid include class="showgrid" in your container element
+"""
 }
 
 setDefaultTarget(main)
