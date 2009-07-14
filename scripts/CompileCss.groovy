f43b96cf11fb70f27ea1801a7c6073bb795c5d6e
altosz <altos.z@gmail.com>
2009-07-14 22:46:03 +0400

    compile-css refactored to use common CompassCompile class

--- scripts/CompileCss.groovy
+++ scripts/CompileCss.groovy
@@ -7,39 +7,15 @@ target(main: "The description of the script goes here!") {
 
 target(compileCss: "Compile sass stylesheets") {
 	GroovyClassLoader loader = new GroovyClassLoader(getClass().getClassLoader())
-	Class clazz = loader.parseClass(new File("$basedir/grails-app/conf/CompassConfig.groovy"))
-	def config = new ConfigSlurper().parse(clazz)
+	
+	Class configClazz = loader.parseClass(new File("$basedir/grails-app/conf/CompassConfig.groovy"))
+	def config = new ConfigSlurper().parse(configClazz)
+	
+	Class compassCompile = loader.parseClass(
+		new File("$compassCssPluginDir/src/groovy/CompassCompile.groovy"))
 
-	def sass_dir = config.compass?.sass_dir
-	def css_dir = config.compass?.css_dir
-	def images_dir = config.compass?.images_dir
-	def relative_assets = config.compass?.relative_assets == null ? true : config.compass?.relative_assets
-	def output_style = config.compass?.output_style ?: 'compact'
-	
-	check sass_dir, "sass_dir is not set (CompassConfig.groovy)"
-	check css_dir, "css_dir is not set (CompassConfig.groovy)"
-	check images_dir, "images_dir is not set (CompassConfig.groovy)"
-	check output_style, "output_style is not set (CompassConfig.groovy)"
-	
-	println """
-sass_dir = '${sass_dir}'"
-css_dir = '${css_dir}'
-images_dir = '${images_dir}'
-relative_assets = ${relative_assets}
-output_style = ${output_style}
-"""
-	
-	println "Compiling sass stylesheets..."
-	
-	ant.exec(executable: "compass") {
-		def relative_assets_arg = relative_assets ? "--relative-assets" : ""
-		arg(line: "--sass-dir ${sass_dir} --css-dir ${css_dir} --images-dir ${images_dir} ${relative_assets_arg} --output-style ${output_style}")
-	}
-}
-
-def check(value, msg) {
-	if (!value) {
-		event("StatusError", [msg])		
+	compassCompile.compile(config, ant) { msg ->
+		event("StatusError", [msg])
 		exit(-1)
 	}
 }
