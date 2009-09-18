3d317e50b62d55ed352184bd16c1705400980040
altosz <altos.z@gmail.com>
2009-09-18 20:14:37 +0400

    Console messages updated with necessary info

--- scripts/_CompassFrameworks.groovy
+++ scripts/_CompassFrameworks.groovy
@@ -5,15 +5,15 @@ availableCompassFrameworks = [
 		description: "Blueprint is a CSS framework, which aims to cut down on your development time. It gives you a solid foundation to build your project on top of, with an easy-to-use grid, sensible typography, useful plugins.",
 		documentation: "http://wiki.github.com/chriseppstein/compass/blueprint-documentation",
 		dir: "blueprint",
-		import: """
+		import: '''
 <head>
-  <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
-  <link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+  <link href="${resource(dir:\'css\',file:\'screen.css\')}" media="screen, projection" rel="stylesheet" type="text/css" />
+  <link href="${resource(dir:\'css\',file:\'print.css\')}" media="print" rel="stylesheet" type="text/css" />
   <!--[if lt IE 8]>
-      <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <link href="${resource(dir:\'css\',file:\'ie.css\')}" media="screen, projection" rel="stylesheet" type="text/css" />
   <![endif]-->
 </head>
-"""
+'''
 	],
 	
 	
@@ -21,11 +21,11 @@ availableCompassFrameworks = [
 		description: "Port of YUI's CSS to Sass",
 		documentation: "http://wiki.github.com/chriseppstein/compass/yui-documentation",
 		dir: "yui",
-		import: """
+		import: '''
 <head>
-  <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <link href="${resource(dir:\'css\',file:\'screen.css\')}" media="screen, projection" rel="stylesheet" type="text/css" />
 </head>
-"""
+'''
 	]
 	
 	
