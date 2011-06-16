eea0b5d953997704fda6c40cf839498710e334c6
U-stefan-lapto\stefan <stefan@stefan-lapto.(none)>
2011-06-15 20:47:41 -0400

    Fixed thread management.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -9,26 +9,26 @@ private ConfigObject getGrassConfig() {
 }
 
 private def startCompassSassWatcher(def grassConfig) {
-    Thread.start {
-        def watchCommand = ['jruby', '-S', 'compass', 'watch', '--sass-dir', grassConfig.sass_dir,
-                '--css-dir', grassConfig.css_dir, '--images-dir', grassConfig.images_dir,
-                '--output-style', grassConfig.output_style]
+    def watchCommand = ['jruby', '-S', 'compass', 'watch', '--sass-dir', grassConfig.sass_dir,
+            '--css-dir', grassConfig.css_dir, '--images-dir', grassConfig.images_dir,
+            '--output-style', grassConfig.output_style]
 
-        println "Starting compass sass watcher: ${watchCommand.join(' ')}"
+    println "Starting compass sass watcher: ${watchCommand.join(' ')}"
 
-        Process executingProcess = watchCommand.execute()
+    Process executingProcess = watchCommand.execute()
 
-        Runtime.runtime.addShutdownHook {
-            println 'Stopping compass sass watcher.'
-            executingProcess.destroy()
-        }
+    GroovyClassLoader loader = new GroovyClassLoader(getClass().getClassLoader())
+    Class streamFilePrinter = loader.parseClass(new File("$grassPluginDir/src/groovy/StreamFilePrinter.groovy"))
 
-        GroovyClassLoader loader = new GroovyClassLoader(getClass().getClassLoader())
-        Class streamFilePrinter = loader.parseClass(new File("$grassPluginDir/src/groovy/StreamFilePrinter.groovy"))
+    def outputFile = new File("compass.out")
+    Thread errorStreamPrinter = streamFilePrinter.newInstance(executingProcess.err, outputFile)
+    Thread outputStreamPrinter = streamFilePrinter.newInstance(executingProcess.in, outputFile)
+    [errorStreamPrinter, outputStreamPrinter]*.start()
 
-        def outputFile = new File("compass.out")
-        def errorStreamPrinter = streamFilePrinter.newInstance(executingProcess.err, outputFile)
-        def outputStreamPrinter = streamFilePrinter.newInstance(executingProcess.in, outputFile)
-        [errorStreamPrinter, outputStreamPrinter]*.start()
+    Runtime.runtime.addShutdownHook {
+        println 'Stopping compass sass watcher.'
+        executingProcess?.destroy()
+        outputStreamPrinter?.stop()
+        errorStreamPrinter?.stop()
     }
 }
